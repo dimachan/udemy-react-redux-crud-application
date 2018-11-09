5186b781f91137cf6d540da4bd63854d28404a66
Daiki Imanaka <dimanaka@icloud.com>
2018-11-10 08:49:33 +0900

    インデント修正

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,14 @@ class App extends Component {
 			<React.Fragment>
 				<div>value: { props.value }</div>
 				<button onClick={props.increment}>+1</button>
-				<button onClick={props.decrement}>-1</button>	
+				<button onClick={props.decrement}>-1</button>
 			</React.Fragment>
 		)
 	}
 }
 
 const mapStateToProps = state => ({ value: state.count.value })
-//const mapDispatchToProps = dispatch => ({ 
+//const mapDispatchToProps = dispatch => ({
 //	increment: () => dispatch(increment()),
 //	decrement: () => dispatch(decrement())
 //})
